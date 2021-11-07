bb839c95e0111374ab7d12e7a906c70111dd62fb
Ariejan de Vroom <ariejan@devroom.io>
2021-11-07 11:47:25 +0100

    Don't re-download existing data

--- yahoo.py
+++ yahoo.py
@@ -1,6 +1,7 @@
-import yfinance as yf
-from pathlib import Path
+import os
 import json
+from pathlib import Path
+import yfinance as yf
 
 # Make sure our output dir exists
 output_dir = "data"
@@ -9,11 +10,13 @@ Path(output_dir).mkdir(exist_ok=True)
 with open("tickers.txt", "r") as tickers:
     for raw_ticker in tickers:
         ticker = raw_ticker.strip()
+        output_file = output_dir + "/" + ticker + ".json"
 
-        print("Loading data for " + ticker)
+        if os.path.isfile(output_file):
+            print("Skipping data for " + ticker)
+        else:
+            print("Fetching data for " + ticker)
+            data = yf.Ticker(ticker)
+            history = data.history(period="max")
 
-        data = yf.Ticker(ticker)
-        history = data.history(period="max")
-
-        print("Writing data for " + ticker)
-        history.to_json(path_or_buf=output_dir + "/" + ticker + ".json", orient="table")
+            history.to_json(path_or_buf=output_file, orient="table")
